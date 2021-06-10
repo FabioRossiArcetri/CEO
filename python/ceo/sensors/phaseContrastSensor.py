976a44d8e911c5ce2a1b6e9a1d306818371bfddc
Teusia <annelaure.cheffot@orange.fr>
2021-06-10 20:22:35 +0200

    rewrote the propagate function to be ceo compatible, also process (almost) do not know what to put in analyse and calibrate functions, wrote a small function that add readout noise and poisson noise to frames.

--- python/ceo/sensors/phaseContrastSensor.py
+++ python/ceo/sensors/phaseContrastSensor.py
@@ -36,6 +36,9 @@ class phaseContrastSensor(object):
         self.reconstructionMatrix = None
         self.reconstructionVector = None
         self.commandVector = None
+        self.curWavelength = None
+        self.noMask = False
+        self.fluxPerMs = None
         
     
     def reset(self):
@@ -72,7 +75,7 @@ class phaseContrastSensor(object):
         return fileName
         
     
-    def normaliseFrame(self,frame,expTimeMs):
+    def normaliseFrame(self,frame,expTimeMs = 1):
         """Normalise the frame given in input
         
         input: a frame
@@ -81,10 +84,10 @@ class phaseContrastSensor(object):
         if not isinstance(self.referenceFrame , np.ndarray):
             print("you first need to create the reference frame, see method setReference")
             return 0
-        self.normalisEDFrame = frame - (self.referenceFrame*expTimeMs)
+        self.normalisEDFrame = frame - (self.referenceFrame*expTimeMs*self.fluxPerMs)
         return 1
     
-    def propagate(self,fluxPerMs,curWavelength = None,noMask = False):
+    def propagate(self,wavefrontObject):
         """does the propagation into the phase contrast sensor and produces an image of the phase contrast 
         
         input: telescope wavefront, flux per unit of ms for the whole collecting area, telescope pupil
@@ -92,8 +95,15 @@ class phaseContrastSensor(object):
         ouput: a frame
         
         """
-        if curWavelength == None:
-            curWavelength = self.wavelength
+        #turn the latest wavefront into a poppy compatible fits
+        self.poppyFits(wavefrontObject.phase.host())
+        
+        if self.fluxPerMs == None:#I need a flux! If you did not set it up I put magic numbers in it
+            #fluxPerMs = Number of Photons * telescope collecting area * 1 ms * telescope throuput * detector quantum efficiency
+            self.fluxPerMs = wavefrontObject.nPhoton[0] * 357.0 * 10**-3 * 0.9**4 * 0.5
+        
+        if self.curWavelength == None:
+            self.curWavelength = self.wavelength
             
         gmtpupil = poppy.FITSOpticalElement(transmission = self.fitsPupilFile,pixelscale = "PIXELSCL")
         
@@ -102,54 +112,52 @@ class phaseContrastSensor(object):
         
         det = poppy.Detector(name='detector', pixelscale=self.telescopeDiameter/self.nPix, fov_pixels=self.nPix)
         
-        if noMask == False:
+        if self.noMask == False:
             pm = poppy.CircularPhaseMask(name = 'zeus',radius = self.maskRadius* \
                                           (self.wavelength/self.telescopeDiameter)*180*3600/np.pi, \
-                                              retardance = self.maskDelay*self.wavelength/curWavelength)
+                                              retardance = self.maskDelay*self.wavelength/self.curWavelength)
         
         osys = poppy.OpticalSystem(oversample = self.poppyOverSampling, npix = self.nPix)
         osys.add_pupil(gmtpupil)
         if self.fitsPhaseFile != None:
             osys.add_pupil(telPhase)
-        if noMask == False:
+        if self.noMask == False:
             osys.add_image(pm)
         else:
             osys.add_image()
         
         osys.add_pupil(det)
 
-        poppyFrame = osys.calc_psf(wavelength = curWavelength)
-        self.frame += poppyFrame[0].data * fluxPerMs
-        return 1
+        poppyFrame = osys.calc_psf(wavelength = self.curWavelength)
+        self.frame += poppyFrame[0].data * self.fluxPerMs
+
     
-    def longExposure(self, expo):
-        """take the latest phase from gmt use it to propagate through the phase contrast sensor and 
-        add the newly created frame to the long exposure frame
-        
-        input: wavefront from the telescope
-        optional input:
-        output: updated long exposure frame"""
-        
-        
     
-    def camera(self):
+    def cameraNoise(self, RON,bias):
         """This is meant to simulate photon noise and various camera noises
         (and introduces photon noise by default)
         input: frame, 
         optional input : RON, background noise, EMCCD Excessnoise
         output: noisy image
         """
-        self.frame *= np.random.poisson(self.frame)
+        rng = np.random.default_rng()
+        
+        self.frame *= rng.poisson(self.frame)
+        self.frame += rng.normal(loc = bias, scale = RON)
+        
+        
     
-    def setReference(self,flatWavefront,flux):
+    def setReference(self,wavefrontObject):
         """ calculate the reference image of the perfect telescope without noise
         
         input: wavefront of the perfect telescope
         output: image of the perfect telescope through the phase contrast
         """
-        self.poppyFits(flatWavefront)
-        self.propagate(flux)
+        tmpFlux = self.fluxPerMs.copy()
+        self.fluxPerMs = 1
+        self.propagate(wavefrontObject)
         self.referenceFrame = self.frame.copy()
+        self.fluxPerMs = tmpFlux.copy()
         return 1
         
     
@@ -169,77 +177,33 @@ class phaseContrastSensor(object):
         
         
         
-    def calibrate(self, strokes, flux, nseg, wavefrontObject,telescopeObject,gmtStandardIM = True, gmtWithTrussMask = []):
-        """creates the interaction matrix of the phase contrast sensor
-        and use it to do the reconstruction matrix
-        
-        input: piston to be applied to build the interaction matrix, the flux of the star, the number of segments, the wavefront object, the telescope object
-        optionnal input: a flag describing whether to use the gmt reconstruction matrix protocole or not
-        output: reconstruction matrix
+    def calibrate(self, ):
+        """Unsure what this should do
         """
-        if gmtStandardIM:
-            if gmtWithTrussMask is []:
-                print("Warning: gmtMaskWithTruss not provided \n Assuming the last amplitude in wavefront object contains a uniformly lit and contain the truss " )
-                gmtWithTrussMask = wavefrontObject.amplitude.host()
-            tmpfitsPupFile = self.fitsPupilFile
-            telescopeObject.project_truss_onaxis = False
-            wavefrontObject.reset()
-            telescopeObject.reset()
-            telescopeObject.propagate(gs)
-            
-            self.poppyFits(wavefrontObject.amplitude.host() \
-                                        ,fileName='noTrussGMTMask.fits', typeOfArray= 'amplitude')
         
-        self.interactionMatrix  = np.zeros((nseg,self.nPix**2))
-        for s in range(nseg):
-            #set up the wavefront to feed to the propagation
-            wavefrontObject.reset()
-            telescopeObject.reset()
-            self.reset()
-            telescopeObject.M2.modes.a[s,0] = strokes*10**-9
-            telescopeObject.M2.modes.update()
-            telescopeObject.propagate(wavefrontObject)
-            
-            #turn the wavefront into a poppy compatible format
-            self.poppyFits(wavefrontObject.phase.host())
-            
-            # feed the wavefront to the propagate method
-            self.propagate(flux)
-            
-            if gmtStandardIM:#if we removed it replace the truss on the images
-                self.frame *= gmtWithTrussMask
-            errorReturn = self.normaliseFrame(self.frame,1)
-            self.interactionMatrix[s] = self.normalisEDFrame.reshape(-1)/strokes
-        
-        self.reconstructionMatrix = np.linalg.pinv(self.interactionMatrix)
-        
-        if gmtStandardIM:
-            self.fitsPupilFile = tmpfitsPupFile
-            telescopeObject.project_truss_onaxis = True
-        
-        return 1
         
         
     def analyse(self, frame, expTimeMs):
-        """analyse the image and return the reconstruction vector
+        """Unsure what this should do"""
+
+
         
-        input: an image array, the reconstruction matrix
-        optionnal input:
-        output: a vector of piston per segments"""
-        if not isinstance(self.reconstructionMatrix, np.ndarray):
-            print("error: requires the reconstruction matrix, please execute method calibrate")
+    def process(self,exposureTime):
+        """Compute the normalised image from which one can retrieve the measurement
+        
+        input: the exposure time (to scale the reference image)
+        optional input:
+        output: the normalised image
+        """
+        if not isinstance(self.referenceFrame , np.ndarray):
+            print("you first need to create the reference frame, see method setReference")
             return 0
-        errorReturn = self.normaliseFrame(frame, expTimeMs)
-        self.reconstructionVector = self.normalisEDFrame.reshape(-1) @ self.reconstructionMatrix
-        self.reconstructionVector -= self.reconstructionVector[0]
+        self.frame = self.frame - (self.referenceFrame*exposureTime*self.fluxPerMs)
+        # self.frame = self.frame - (self.referenceFrame*np.sum(self.frame))
         return 1
-        
-    def commandVector(self):
-        """take the reconstruction vector and turn it into a CEO compatible command vector
-        
-        input: the reconstruction vector
-        optionnal input:
-        output: the command vector for the NGAO"""
+    
+    
+
     
         
         
@@ -273,6 +237,7 @@ if __name__=='__main__':
     detectorQE = 0.5
     
     simul_truss_mask=True  # If True, it applies to closed loop simulation, and slope-null calibration only (NOT IM calibration)
+    gmtStandardIM = True
     
     #base configuration of the wavefront
     wl2nd = 850e-9
@@ -296,9 +261,11 @@ if __name__=='__main__':
     
     #create the object for the phase contrast
     zelda = phaseContrastSensor(2,0.225,wl2nd,nPx)
-    
+    strokes = 25 #later for interaction matrix
+    zelda.fluxPerMs = nPhotPerMs
     #first create the pupil mask (needed for poppy)
     
+    #This is a prerequisite for poppy to work. Maybe that should be part of a method?
     gmt.reset()
     gs.reset()
     zelda.reset()
@@ -311,7 +278,7 @@ if __name__=='__main__':
     fileName = zelda.poppyFits(gmtMask,'GMTpupil.fits', 'transmision')
     
     #does a frame without piston with mask
-    toto = zelda.propagate(1,noMask=False)
+    toto = zelda.propagate(gs)
     plt.figure(2)
     plt.clf()
     plt.imshow(zelda.frame*np.sum(gmtMask))
@@ -324,13 +291,8 @@ if __name__=='__main__':
     gmt.M2.modes.update()
     gmt.propagate(gs)
     
-    #extract the phase out of this
-    curPhase = gs.phase.host()
-    
-    #turn the phase into a poppy compatible format
-    fileName = zelda.poppyFits(curPhase)
-    #calculate a frame with this phase 
-    toto = zelda.propagate(nPhotPerMs)
+    #calculate a frame with the current phase contained in gs
+    zelda.propagate(gs)
     
     plt.figure(3)
     plt.clf()
@@ -344,19 +306,27 @@ if __name__=='__main__':
     gmt.propagate(gs)
     
     #now we feed the wavefront to the object
-    zelda.setReference(gs.phase.host(), 1)
+    zelda.setReference(gs)
     plt.figure(4)
     plt.clf()
     plt.imshow(zelda.referenceFrame)
+    plt.title("reference image")
     
     #next is the normalisation function, at this point you should already have created 
     #at least the reference frame
     #first we want to test with the flat wavefront
-    zelda.normaliseFrame(zelda.frame, 1)
+    
+    gmt.reset()
+    gs.reset()
+    zelda.reset()
+    gmt.propagate(gs)
+    zelda.propagate(gs)
+    
+    zelda.process(1)
     plt.figure(5)
     plt.clf()
-    plt.imshow(zelda.normalisEDFrame)
-    plt.title("this image should be empty")
+    plt.imshow(zelda.frame)
+    plt.title("this image should be full of zeros")
     
     #now repeat this with a piston on one segment
     gmt.reset()
@@ -365,18 +335,52 @@ if __name__=='__main__':
     gmt.M2.modes.a[0,0] = 200*10**-9
     gmt.M2.modes.update()
     gmt.propagate(gs)
-    fileName = zelda.poppyFits(gs.phase.host())
-    #calculate a frame with this phase 
-    toto = zelda.propagate(1)
+
+    #calculate a frame with the latest frame and add it to itself
+    zelda.propagate(gs)
     
-    zelda.normaliseFrame(zelda.frame,1)
+    zelda.process(1)
     
     plt.figure(6)
     plt.clf()
-    plt.imshow(zelda.normalisEDFrame)
+    plt.imshow(zelda.frame)
     
     # next we create the reconstruction matrix accoring to GMT standards
-    zelda.calibrate(25,1,nseg,gs,gmt,gmtWithTrussMask=gmtMask)
+    
+    if gmtStandardIM:
+        tmpfitsPupFile = zelda.fitsPupilFile
+        gmt.project_truss_onaxis = False
+        gs.reset()
+        gmt.reset()
+        gmt.propagate(gs)
+        
+        zelda.poppyFits(gs.amplitude.host() \
+                                    ,fileName='noTrussGMTMask.fits', typeOfArray= 'amplitude')
+    
+    interactionMatrix  = np.zeros((nseg,nPx**2))
+    for s in range(nseg):
+        #set up the wavefront to feed to the propagation
+        gs.reset()
+        gmt.reset()
+        zelda.reset()
+        gmt.M2.modes.a[s,0] = strokes*10**-9
+        gmt.M2.modes.update()
+        gmt.propagate(gs)
+        
+        # feed the wavefront to the propagate method
+        zelda.propagate(gs)
+        
+        if gmtStandardIM:#if we removed it replace the truss on the images
+            zelda.frame *= gmtMask
+        errorReturn = zelda.process(1)
+        interactionMatrix[s] = zelda.frame.reshape(-1)/strokes
+    
+    reconstructionMatrix = np.linalg.pinv(interactionMatrix)
+    
+    if gmtStandardIM:
+        zelda.fitsPupilFile = tmpfitsPupFile
+        gmt.project_truss_onaxis = True
+    
     
     #next we generate a random piston on a random segment and calculate the reconstructed piston
     randSeg = np.random.randint(0,7)
@@ -389,13 +393,16 @@ if __name__=='__main__':
     gmt.M2.modes.a[randSeg,0] = randpist
     gmt.M2.modes.update()
     gmt.propagate(gs)
-    fileName = zelda.poppyFits(gs.phase.host())
     
-    zelda.propagate(1)
+    zelda.process(expTimeMs)
+    reconstructionVector = zelda.frame.reshape(-1) @ reconstructionMatrix
+    reconstructionVector -= reconstructionVector[0]
+    
+    zelda.propagate(gs)
     zelda.analyse(zelda.frame,1)
-    print(zelda.reconstructionVector)
+    print(reconstructionVector)
     
-    #lets do a sweep for the fun of it
+    # #lets do a sweep for the fun of it
     pist2apply = np.arange(-3*750,3*751,750/8)*10**-9
     randSeg = 4
     res = []
@@ -406,13 +413,14 @@ if __name__=='__main__':
         gmt.M2.modes.a[randSeg,0] = p
         gmt.M2.modes.update()
         gmt.propagate(gs)
-        fileName = zelda.poppyFits(gs.phase.host())
         
-        zelda.propagate(1)
-        zelda.analyse(zelda.frame,1)
-        res.append(zelda.reconstructionVector)
+        zelda.propagate(gs)
+        zelda.process(1)
+        reconstructionVector = zelda.frame.reshape(-1) @ reconstructionMatrix
+        reconstructionVector -= reconstructionVector[0]
+        res.append(reconstructionVector)
         
-    # res = np.array(res)
+    # # res = np.array(res)
     #and display how the result went
     plt.figure(7)
     plt.clf()
@@ -424,15 +432,19 @@ if __name__=='__main__':
         gmt.reset()
         gs.reset()
         gmt.propagate(gs)
-        fileName = zelda.poppyFits(gs.phase.host())
-        zelda.propagate(nPhotPerMs)
+        zelda.propagate(gs)
     
     
-    plt.figure(8)
+    plt.figure(8,figsize=(12,8))
     plt.clf()
-    fig,axs = plt.subplots(num = 8, nrows=1,ncols=2)
+    fig,axs = plt.subplots(num = 8, nrows=1,ncols=3)
     axs[0].imshow(zelda.frame)
     
-    zelda.camera()
+    zelda.cameraNoise(10,0)
     axs[1].imshow(zelda.frame)
+    
+    #try to normalise the now noisy image
+    zelda.process(expTimeMs)
+    axs[2].imshow(zelda.frame)
+    
     
